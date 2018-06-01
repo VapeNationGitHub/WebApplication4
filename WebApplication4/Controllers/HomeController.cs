04abebc3d1d98b12829514be573e9319e87e1d07
Nikita Ponomarev <iponomar47@gmail.com>
2018-06-01 11:53:41 +0800

    123

--- WebApplication4/Controllers/HomeController.cs
+++ WebApplication4/Controllers/HomeController.cs
@@ -1,16 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace WebApplication4.Controllers
 {
     public class HomeController : Controller
     {
+
+
+        public string Get()
+        {
+
+
+            string browser = HttpContext.Request.Browser.Browser;
+            string user_agent = HttpContext.Request.UserAgent;
+            string url = HttpContext.Request.RawUrl;
+            string ip = HttpContext.Request.UserHostAddress;
+            string referrer = HttpContext.Request.UrlReferrer == null ? "" : HttpContext.Request.UrlReferrer.AbsoluteUri;
+            return "<p>Browser: " + browser + "</p><p>User-Agent: " + user_agent + "</p><p>Url запроса: " + url +
+                "</p><p>Реферер: " + referrer + "</p><p>IP-адрес: " + ip + "</p>";
+
+            /*
+            // Создать объект cookie-набора
+            HttpCookie cookie = new HttpCookie("My localhost cookie");
+            // Установить значения в нем
+            cookie["Language"] = "Russian";
+            cookie["Country"] = "ru-ru";
+            // Добавить куки в ответ
+            Response.Cookies.Add(cookie);
+            cookie.Expires = DateTime.Now.AddYears(1);
+            */
+        }
+        // request - запрос
+        // response - ответ
+
         public ActionResult Index()
         {
+
+
+
+           
+
+            /*
+            // Создать объект cookie-набора
+            HttpCookie cookie = new HttpCookie("My localhost cookie");
+            // Установить значения в нем
+            cookie["Language"] = "Russian";
+            cookie["Country"] = "ru-ru";
+            // Добавить куки в ответ
+            Response.Cookies.Add(cookie);
+            cookie.Expires = DateTime.Now.AddYears(1);
+            */
+
+
+
             return View();
+            
         }
 
         public ActionResult About()
@@ -20,11 +72,19 @@ namespace WebApplication4.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public string Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            
+            string browser = HttpContext.Request.Browser.Browser;
+            string user_agent = HttpContext.Request.UserAgent;
+            string url = HttpContext.Request.RawUrl;
+            string ip = HttpContext.Request.UserHostAddress;
+            string referrer = HttpContext.Request.UrlReferrer == null ? "" : HttpContext.Request.UrlReferrer.AbsoluteUri;
+            return "<p>Browser: " + browser + "</p><p>User-Agent: " + user_agent + "</p><p>Url запроса: " + url +
+                "</p><p>Реферер: " + referrer + "</p><p>IP-адрес: " + ip + "</p>";
+                
+            //return View();
         }
+
     }
 }
